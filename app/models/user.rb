c78f55beb0d7a572eef74be30f289dfbf554f780
sean-garwood <seansgarwood@gmail.com>
2024-06-13 09:04:23 -0400

    feat: comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_many :posts
-  has_many :comments, through: :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_13_105402) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_13_125627) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "url"
@@ -27,5 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_105402) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
